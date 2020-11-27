ea5ce040badda995beb938478861339fa56e9c6c
Arturo Carrera <amerik0904@gmail.com>
2020-11-27 14:59:57 -0400

    side bar

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -104,109 +104,109 @@
 // }
 
 
-import React, { useState, useRef} from 'react';
-import FASearch from 'react-icons/lib/fa/search'
-import SideBarFnOption from './SideBarOption'
-import { last, get, differenceBy } from 'lodash' 
-import { createChatNameFromUsers } from '../../Factories'
-import MdEject from 'react-icons/lib/md/eject'
+// import React, { useState, useRef} from 'react';
+// import FASearch from 'react-icons/lib/fa/search'
+// import SideBarFnOption from './SideBarOption'
+// import { last, get, differenceBy } from 'lodash' 
+// import { createChatNameFromUsers } from '../../Factories'
+// import MdEject from 'react-icons/lib/md/eject'
 
-function SideBarFn(props){
-	const [state, setState]= useState({
-		reciever:"",
-		activeSideBarFn: SideBarFn.type.CHATS
-	})
+// function SideBarFn(props){
+// 	const [state, setState]= useState({
+// 		reciever:"",
+// 		activeSideBarFn: SideBarFn.type.CHATS
+// 	})
 
-	const inputRef = useRef(null);
+// 	const inputRef = useRef(null);
 
-	const handleSubmit = (e) =>{
-		e.preventDefault()
-		const { reciever } = state
-		const { onSendPrivateMessage } = props
+// 	const handleSubmit = (e) =>{
+// 		e.preventDefault()
+// 		const { reciever } = state
+// 		const { onSendPrivateMessage } = props
 
-		onSendPrivateMessage(reciever)
-		setState({...state, reciever:reciever})
-	}
+// 		onSendPrivateMessage(reciever)
+// 		setState({...state, reciever:reciever})
+// 	}
 
-	const addChatForUser = (reciever) =>{
-		props.onSendPrivateMessage(reciever)
-		setActiveSideBarFn(SideBarFn.type.CHATS)
-	}
+// 	const addChatForUser = (reciever) =>{
+// 		props.onSendPrivateMessage(reciever)
+// 		setActiveSideBarFn(SideBarFn.type.CHATS)
+// 	}
 
-	const setActiveSideBarFn = (type) =>{
-		setState({...state, activeSideBarFn:type })
-	}
+// 	const setActiveSideBarFn = (type) =>{
+// 		setState({...state, activeSideBarFn:type })
+// 	}
 
-	return (
-			<div id="side-bar">
-					<div className="heading">
+// 	return (
+// 			<div id="side-bar">
+// 					<div className="heading">
 						
-					</div>
-					<form onSubmit={handleSubmit} className="search">
-						<i className="search-icon"><FASearch /></i>
-						<input 
-							placeholder="Search" 
-							type="text"
-							value={state.reciever}
-							onChange={(e)=>{ setState({reciever:e.target.value}) }}/>
-					</form>
-					<div className="side-bar-select">
-						<div 
-							onClick = { ()=>{ setActiveSideBarFn(SideBarFn.type.CHATS) } }
-							className={`side-bar-select__option ${ state.activeSideBarFn === SideBarFn.type.CHATS ? 'active':''}`}>
-							<span>Chats</span>
-						</div>
-						<div 
-							onClick = { ()=>{ setActiveSideBarFn(SideBarFn.type.USERS) } }
-							className={`side-bar-select__option ${ state.activeSideBarFn === SideBarFn.type.USERS ? 'active':''}`}>
-							<span>Users</span>
-						</div>
-					</div>
-					<div 
-						className="users" 
-						ref={inputRef} 
-						onClick={(e)=>{ (e.target === inputRef.user) && props.setActiveChat(null) }}>
+// 					</div>
+// 					<form onSubmit={handleSubmit} className="search">
+// 						<i className="search-icon"><FASearch /></i>
+// 						<input 
+// 							placeholder="Search" 
+// 							type="text"
+// 							value={state.reciever}
+// 							onChange={(e)=>{ setState({reciever:e.target.value}) }}/>
+// 					</form>
+// 					<div className="side-bar-select">
+// 						<div 
+// 							onClick = { ()=>{ setActiveSideBarFn(SideBarFn.type.CHATS) } }
+// 							className={`side-bar-select__option ${ state.activeSideBarFn === SideBarFn.type.CHATS ? 'active':''}`}>
+// 							<span>Chats</span>
+// 						</div>
+// 						<div 
+// 							onClick = { ()=>{ setActiveSideBarFn(SideBarFn.type.USERS) } }
+// 							className={`side-bar-select__option ${ state.activeSideBarFn === SideBarFn.type.USERS ? 'active':''}`}>
+// 							<span>Users</span>
+// 						</div>
+// 					</div>
+// 					<div 
+// 						className="users" 
+// 						ref={inputRef} 
+// 						onClick={(e)=>{ (e.target === inputRef.user) && props.setActiveChat(null) }}>
 						
-						{
-						state.activeSideBarFn === SideBarFn.type.CHATS ?
-						props.chats.map((chat)=>{
-								return(
-								<SideBarFnOption 
-									key = {chat.id}
-									lastMessage = { get(last(chat.messages), 'message', '') }
-									name = { chat.isCommunity ? chat.name : createChatNameFromUsers(chat.users, props.user.name) }
-									active = { props.activeChat.id === chat.id }
-									onClick = { ()=>{ props.setActiveChat(chat) } }
-								/>
-							)
-						})	
+// 						{
+// 						state.activeSideBarFn === SideBarFn.type.CHATS ?
+// 						props.chats.map((chat)=>{
+// 								return(
+// 								<SideBarFnOption 
+// 									key = {chat.id}
+// 									lastMessage = { get(last(chat.messages), 'message', '') }
+// 									name = { chat.isCommunity ? chat.name : createChatNameFromUsers(chat.users, props.user.name) }
+// 									active = { props.activeChat.id === chat.id }
+// 									onClick = { ()=>{ props.setActiveChat(chat) } }
+// 								/>
+// 							)
+// 						})	
 						
-						:
-							differenceBy(props.users, [props.user], 'name').map((user)=>{
-								return <SideBarFnOption 
-									key = { user.id }
-									name = { user.name }
-									onClick = { ()=>{ addChatForUser(user.name) }  }
-								/>
-							})
-						}
-					</div>
-					<div className="current-user">
-						<span>{props.user.name}</span>
-						<div onClick={()=>{props.logout()}} title="Logout" className="logout">
-							<MdEject/>	
-						</div>
-					</div>
-			</div>
-		);
-}
+// 						:
+// 							differenceBy(props.users, [props.user], 'name').map((user)=>{
+// 								return <SideBarFnOption 
+// 									key = { user.id }
+// 									name = { user.name }
+// 									onClick = { ()=>{ addChatForUser(user.name) }  }
+// 								/>
+// 							})
+// 						}
+// 					</div>
+// 					<div className="current-user">
+// 						<span>{props.user.name}</span>
+// 						<div onClick={()=>{props.logout()}} title="Logout" className="logout">
+// 							<MdEject/>	
+// 						</div>
+// 					</div>
+// 			</div>
+// 		);
+// }
 
-SideBarFn.type = {
-		USERS:"users",
-		CHATS:"chats"
-}
+// SideBarFn.type = {
+// 		USERS:"users",
+// 		CHATS:"chats"
+// }
 
-export default (SideBarFn);
+// export default (SideBarFn);
 
 // import React, { useState, useRef} from 'react';
 // import FASearch from 'react-icons/lib/fa/search'
@@ -282,3 +282,85 @@ export default (SideBarFn);
 // }
 
 // export default (withSideBar(SideBar));
+
+// AMERICA ////
+
+import React, { useState, useRef} from 'react';
+import FASearch from 'react-icons/lib/fa/search'
+import SideBarFnOption from './SideBarOption'
+import { last, get, differenceBy } from 'lodash' 
+import { createChatNameFromUsers } from '../../Factories'
+import MdEject from 'react-icons/lib/md/eject'
+import withSideBar from './withSideBar';
+
+const SideBarFn = props => {
+	
+	return (
+		<div id="side-bar">
+				<div className="heading">
+					
+				</div>
+				<form onSubmit={props.handleSubmit} className="search">
+					<i className="search-icon"><FASearch /></i>
+					<input 
+						placeholder="Search" 
+						type="text"
+						value={props.reciever}
+						onChange={props.handleChangeReciever}/>
+				</form>
+				<div className="side-bar-select">
+					<div 
+						onClick = { ()=>{ props.setActiveSideBarFn(SideBarFn.type.CHATS) } }
+						className={`side-bar-select__option ${ props.activeSideBarFn === SideBarFn.type.CHATS ? 'active':''}`}>
+						<span>Chats</span>
+					</div>
+					<div 
+						onClick = { ()=>{ props.setActiveSideBarFn(SideBarFn.type.USERS) } }
+						className={`side-bar-select__option ${ props.activeSideBarFn === SideBarFn.type.USERS ? 'active':''}`}>
+						<span>Users</span>
+					</div>
+				</div>
+				<div 
+					className="users" 
+					ref={props.inputRef} 
+					onClick={(e)=>{ (e.target === props.inputRef.user) && props.setActiveChat(null) }}>
+					
+					{
+					props.activeSideBarFn === SideBarFn.type.CHATS ?
+					props.chats.map((chat)=>{
+							return(
+							<SideBarFnOption 
+								key = {chat.id}
+								lastMessage = { get(last(chat.messages), 'message', '') }
+								name = { chat.isCommunity ? chat.name : createChatNameFromUsers(chat.users, props.user.name) }
+								active = { props.activeChat.id === chat.id }
+								onClick = { ()=>{ props.setActiveChat(chat) } }
+							/>
+						)
+					})	
+					
+					:
+						differenceBy(props.users, [props.user], 'name').map((user)=>{
+							return <SideBarFnOption 
+								key = { user.id }
+								name = { user.name }
+								onClick = { ()=>{ props.addChatForUser(user.name) }  }
+							/>
+						})
+					}
+				</div>
+				<div className="current-user">
+					<span>{props.user.name}</span>
+					<div onClick={()=>{props.logout()}} title="Logout" className="logout">
+						<MdEject/>	
+					</div>
+				</div>
+		</div>
+	);
+}
+
+SideBarFn.type = {
+	USERS:"users",
+	CHATS:"chats"
+}
+export default (withSideBar(SideBarFn));
